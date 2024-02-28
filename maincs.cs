e15da75b7cf499e3c59a6daa6d813dc93eab4188
Benjamin Lamar <161600070+filamelio23@users.noreply.github.com>
2024-02-28 14:46:11 +0200

    Create maincs.cs

---
+++ maincs.cs
@@ -0,0 +1,28 @@
+ if (associatedProducts.Any())
+                {
+                    contextProduct = associatedProducts.OrderBy(x => x.DisplayOrder).First();
+                    _services.DisplayControl.Announce(contextProduct);
+                }
+            }
+            else
+            {
+                priceModel.DisableBuyButton = product.DisableBuyButton || !context.AllowShoppingCart || !context.AllowPrices;
+                priceModel.DisableWishlistButton = product.DisableWishlistButton || !context.AllowWishlist || !context.AllowPrices;
+                priceModel.AvailableForPreOrder = product.AvailableForPreOrder;
+            }
+        public async Task<List<BrandOverviewModel>> PrepareBrandOverviewModelAsync(
+            ICollection<ProductManufacturer> brands,
+            IDictionary<int, BrandOverviewModel> cachedModels = null,
+            bool withPicture = false)
+        {
+            var model = new List<BrandOverviewModel>();
+            cachedModels ??= new Dictionary<int, BrandOverviewModel>();
+            IDictionary<int, MediaFileInfo> mediaFileLookup = null;
+
+            if (withPicture)
+            {
+                var manufacturerFileIds = brands
+                    .Select(x => x.Manufacturer.MediaFileId ?? 0)
+                    .Where(x => x > 0)
+                    .Distinct()
+                    .ToArray();
